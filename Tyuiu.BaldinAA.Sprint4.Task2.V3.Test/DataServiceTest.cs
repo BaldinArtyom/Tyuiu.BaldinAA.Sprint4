87374e818d86f05f219fc93272cf87700521be99
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-11-15 01:30:28 +0500

    Task2

---
+++ Tyuiu.BaldinAA.Sprint4.Task2.V3.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+using Tyuiu.BaldinAA.Sprint4.Task2.V3.Lib;
+namespace Tyuiu.BaldinAA.Sprint4.Task2.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+        }
+    }
+}
