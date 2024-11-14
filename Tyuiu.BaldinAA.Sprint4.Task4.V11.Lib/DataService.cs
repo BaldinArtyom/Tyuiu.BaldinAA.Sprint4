6b36ff926fb2d9f79b73b38c90517ad1a63e67dd
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-11-15 04:01:27 +0500

    Task4

---
+++ Tyuiu.BaldinAA.Sprint4.Task4.V11.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.BaldinAA.Sprint4.Task4.V11.Lib
+{
+    public class DataService : ISprint4Task4V11
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int kols = matrix.GetUpperBound(0) + 1;
+            int kolst = matrix.Length / kols;
+            int job = 0;
+
+            for (int i = 0; i < kols; i++)
+            {
+                for (int j = 0; j < kolst; j++)
+                {
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        job += matrix[i, j];
+                    }
+                }
+            }
+            return job; ;
+        }
+    }
+}
