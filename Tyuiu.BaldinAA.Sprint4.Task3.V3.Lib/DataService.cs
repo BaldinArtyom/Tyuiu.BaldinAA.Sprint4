51831845ef28b95a5860d40ca35be4a13165af7d
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-11-15 01:41:36 +0500

    Task3

---
+++ Tyuiu.BaldinAA.Sprint4.Task3.V3.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.BaldinAA.Sprint4.Task3.V3.Lib
+{
+    public class DataService : ISprint4Task3V3
+    {
+        public int Calculate(int[,] array)
+        {
+            int res = 0;
+            int height = array.GetLength(0);
+            int width = array.GetLength(1);
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (x == 0)
+                    {
+                        res += array[y, 0];
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
