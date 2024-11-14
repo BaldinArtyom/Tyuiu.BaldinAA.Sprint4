55c094fe28b0f8d392edc945fd103b419c7106b2
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-11-15 04:06:53 +0500

    Task5

---
+++ Tyuiu.BaldinAA.Sprint4.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+using Tyuiu.BaldinAA.Sprint4.Task5.V4.Lib;
+namespace Tyuiu.BaldinAA.Sprint4.Task5.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5] { { -4, 3, 5, 1, -2 },
+                                          { 0, -4, 0, 6, 4 },
+                                          { -4, -4, 2, -2, -1 },
+                                          { 4, -3, 4, -1, 6 },
+                                          { 1, 4, -1, 3, 0 } };
+
+            int res = ds.Calculate(mas2);
+            int wait = 12;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BaldinAA.Sprint4.Task5.V4/Program.cs
@@ -0,0 +1,63 @@
+﻿using Tyuiu.BaldinAA.Sprint4.Task5.V4.Lib;
+namespace Tyuiu.BaldinAA.Sprint4.Task5.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4|Выполнил: Балдин А. А. | ИБКСб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы (ввод с клавиатуры).                            *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Балдин Артём Александрович | ИБКСб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 3 до 9.                          *");
+            Console.WriteLine("* Найдите сумму четных элементов массива.                                 *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцов в массиве: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mtrx = new int[rows, columns];
+
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"Введите {i},{j} элемент массива: ");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(mtrx);
+
+            Console.WriteLine("Сумма четных элементов массива = " + res); ;
+        }
+    }
+}
