745144dfaa06182bc28382b685af8b3f96139885
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-11-15 04:12:29 +0500

    Task6

---
+++ Tyuiu.BaldinAA.Sprint4.Task6.V15.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.BaldinAA.Sprint4.Task6.V15.Lib;
+namespace Tyuiu.BaldinAA.Sprint4.Task6.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var week = new string[] { "Чикаго", "Хьюстон", "Феникс", "Филадельфия", "Сан-Антонио", "Сан-Диего", "Даллас" };
+            int res = ds.Calculate(week);
+            int wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
